5e5496be2117659b9c2dc68a25f7d864f2392154
William Denton <wtd@pobox.com>
2016-05-10 09:01:33 -0400

    Rename scripts directory

--- conforguration_scripts/r-install-from-source.sh
+++ conforguration_scripts/r-install-from-source.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+unset RVERSION
+declare -a RVERSION=( '3.3.0' )
 cd /usr/local/src/R
-VERSION=3.3.0
-curl -O http://cran.utstat.utoronto.ca/src/base/R-3/R-$VERSION.tar.gz
-tar xzvf R-$VERSION.tar.gz
-cd R-$VERSION
+curl -O http://cran.utstat.utoronto.ca/src/base/R-3/R-$RVERSION.tar.gz
+tar xzvf R-$RVERSION.tar.gz
+cd R-$RVERSION
 ./configure
 make && make check
 cd ..
 rm R Rscript
-ln -s R-$VERSION/bin/R R
-ln -s R-$VERSION/bin/Rscript Rscript
+ln -s R-$RVERSION/bin/R R
+ln -s R-$RVERSION/bin/Rscript Rscript
 PACKAGE_LIST="dplyr readr tidyr ggplot2 devtools lubridate shiny knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels"
 for PKG in $PACKAGE_LIST; do ./Rscript --vanilla -e "install.packages('$PKG', repos=c('https://cran.hafro.is/'))"; done
 ./Rscript --vanilla -e "devtools::install_github('rstudio/shinyapps')"
