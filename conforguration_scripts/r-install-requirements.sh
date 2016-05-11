865a05923a8791abd269febc5ac61398bea64910
William Denton <wdenton@yorku.ca>
2016-05-11 12:05:49 -0400

    Retangle

--- conforguration_scripts/r-install-requirements.sh
+++ conforguration_scripts/r-install-requirements.sh
@@ -2,4 +2,6 @@
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 sudo apt-get install libbz2-dev liblzma-dev libpcre3-dev fonts-inconsolata
+sudo apt-get install xorg-dev gfortran libreadline-dev libcurl4-openssl-dev
 sudo apt-get install libgsl0-dev
+mkdir -p ~/R/history/
