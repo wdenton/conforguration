a240d9947128011411aa26b2c38eefddc477cf26
William Denton <wtd@pobox.com>
2016-11-29 22:21:09 -0500

    Add Ruby conforg

--- conforguration_scripts/r-install-from-source.sh
+++ conforguration_scripts/r-install-from-source.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
-unset RVERSION
-declare -a RVERSION=( '3.3.2' )
+unset R_VERSION
+declare -a R_VERSION=( '3.3.2' )
 cd /usr/local/src/R
-curl -O https://cran.hafro.is/src/base/R-3/R-$RVERSION.tar.gz
-tar xzvf R-$RVERSION.tar.gz
-cd R-$RVERSION
+curl -O https://cran.hafro.is/src/base/R-3/R-$R_VERSION.tar.gz
+tar xzvf R-$R_VERSION.tar.gz
+cd R-$R_VERSION
 ./configure
 make && make check
 cd ..
 rm R Rscript
-ln -s R-$RVERSION/bin/R R
-ln -s R-$RVERSION/bin/Rscript Rscript
+ln -s R-$R_VERSION/bin/R R
+ln -s R-$R_VERSION/bin/Rscript Rscript
 PACKAGE_LIST="dplyr readr tidyr ggplot2 devtools roxygen2 lubridate shiny knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels"
 for PKG in $PACKAGE_LIST; do ./Rscript --vanilla -e "install.packages('$PKG', repos=c('https://cran.hafro.is/'))"; done
 ./Rscript --vanilla -e "devtools::install_github('rstudio/shinyapps')"
---
+++ conforguration_scripts/ruby-install-from-source.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+script_dir='conforguration_scripts'
+dotfiles_dir='dotfiles'
+unset RUBY_VERSION
+declare -a RUBY_VERSION=( '2.3.3' )
+rm -rf ~/.rbenv/
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bashrc
+rbenv install --verbose $RUBY_VERSION
+rbenv global $RUBY_VERSION
+gem install marc nokogiri sqlite3 rubocop backup bundler jekyll
+echo "Now run bundler install where needed."
