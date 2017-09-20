c4d1bdd0c66bdc9939503bf16207d5f8566fc37b
William Denton <wtd@pobox.com>
2017-09-20 11:50:00 -0400

    Tweak R install

--- conforguration_scripts/r-install-from-source.sh
+++ conforguration_scripts/r-install-from-source.sh
@@ -4,16 +4,27 @@ dotfiles_dir='dotfiles'
 unset R_VERSION
 declare -a R_VERSION=( '3.4.1' )
 cd /usr/local/src/R
+
+# Wipe this version if it is there already
+rm -rf R-$R_VERSION.tar.gz R-$R_VERSION
+
+# Download and uncompress
 curl -O https://cran.hafro.is/src/base/R-3/R-$R_VERSION.tar.gz
 tar xzvf R-$R_VERSION.tar.gz
+
+# Compile
 cd R-$R_VERSION
 ./configure
 make && make check
+
+# Set up aliases
 cd ..
 rm R Rscript
 ln -s R-$R_VERSION/bin/R R
 ln -s R-$R_VERSION/bin/Rscript Rscript
-PACKAGE_LIST="dplyr readr tidyr ggplot2 devtools RCurl roxygen2 testthat lubridate shiny knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels"
+
+# Install all the packages I want available by default
+PACKAGE_LIST="tidyverse devtools RCurl roxygen2 testthat lubridate shiny flexdashboard knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels"
 for PKG in $PACKAGE_LIST; do ./Rscript --vanilla -e "install.packages('$PKG', repos=c('https://cran.hafro.is/'))"; done
 ## ./Rscript --vanilla -e "devtools::install_github('rstudio/shinyapps')"
 ./Rscript --vanilla -e "devtools::install_github('yorkulibraries/yulr')"
