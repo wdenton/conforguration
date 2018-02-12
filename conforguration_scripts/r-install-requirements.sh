83f4e6db5b0db82fbe50feb08496bc68ecba6394
William Denton <wtd@pobox.com>
2018-02-12 13:54:54 -0500

    R tweaks
    To ensure things work on a new server

--- conforguration_scripts/r-install-requirements.sh
+++ conforguration_scripts/r-install-requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
-sudo apt-get install libbz2-dev liblzma-dev libpcre3-dev fonts-inconsolata
+sudo apt-get install libbz2-dev liblzma-dev libxml2-dev libpcre3-dev fonts-inconsolata
 sudo apt-get install xorg-dev gfortran libreadline-dev libcurl4-openssl-dev libssl-dev
 sudo apt-get build-dep r-base
 sudo apt-get install libgsl-dev
