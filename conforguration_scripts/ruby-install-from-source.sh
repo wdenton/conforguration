edec81d12c89e068ff5e5fb7c2db280402899617
William Denton <wtd@pobox.com>
2018-02-13 10:49:08 -0500

    Source install of Ruby system-wide

---
+++ conforguration_scripts/ruby-install-from-source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script_dir='conforguration_scripts'
+dotfiles_dir='dotfiles'
+unset RUBY_VERSION
+declare -a RUBY_VERSION=( '2.5.0' )
+mkdir -p /usr/local/src/ruby
+cd /usr/local/src/ruby
+
+curl -LO https://cache.ruby-lang.org/pub/ruby/2.5/ruby-${RUBY_VERSION}.tar.gz
+tar xzvf ruby-${RUBY_VERSION}.tar.gz
+cd ruby-${RUBY_VERSION}
+
+./configure
+make
+
+sudo make install
+sudo gem install marc nokogiri sqlite3 rubocop bundler rubyul
+
+echo "Now run bundler install where needed."
