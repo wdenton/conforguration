002a0a7bba64ea5a18b36449a11e74fb582aefbd
William Denton <wtd@pobox.com>
2018-02-14 12:18:05 -0500

    Various minor updates and corrections

--- conforguration_scripts/emacs-install-from-source.sh
+++ conforguration_scripts/emacs-install-from-source.sh
@@ -3,17 +3,17 @@ script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 cd /usr/local/src/
 # Emacs
-git clone git://git.savannah.gnu.org/emacs.git
+git clone https://git.savannah.gnu.org/git/emacs.git
 cd emacs
 ./autogen.sh
 ./configure
 make
 # Org
 cd /usr/local/src/
-git clone git://orgmode.org/org-mode.git
+git clone https://code.orgmode.org/bzg/org-mode.git
 cd org-mode
 make
 # My config
 cd
 git clone git@github.com:wdenton/.emacs.d.git
-echo "Now run emacs, and say no when asked about a location for abbref_defs."
+echo "Now run emacs, and say no when asked about a location for abbrev_defs."
--- conforguration_scripts/emacs-install-requirements.sh
+++ conforguration_scripts/emacs-install-requirements.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
-sudo apt-get build-dep emacs24
+sudo apt-get build-dep emacs
