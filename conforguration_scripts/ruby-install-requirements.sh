e8a51261de18d9dd00dd4e222d74ff76047d94d3
William Denton <wtd@pobox.com>
2018-07-18 10:37:24 -0400

    Clean up code duplication in shell scrtips

--- conforguration_scripts/ruby-install-requirements.sh
+++ conforguration_scripts/ruby-install-requirements.sh
@@ -2,7 +2,3 @@
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 sudo apt-get build-dep ruby
-
-script_dir='conforguration_scripts'
-dotfiles_dir='dotfiles'
-sudo apt-get build-dep ruby
