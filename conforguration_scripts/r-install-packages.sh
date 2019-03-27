ea174c0159f281ac5e9ae58c7e4bd074c80c6fb6
William Denton <wtd@pobox.com>
2019-03-27 13:30:07 -0400

    R package: ggrepel

--- conforguration_scripts/r-install-packages.sh
+++ conforguration_scripts/r-install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
-PACKAGE_LIST="tidyverse readxl devtools RCurl roxygen2 testthat lubridate shiny flexdashboard knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels xlsx lintr fs";
+PACKAGE_LIST="tidyverse readxl devtools RCurl roxygen2 testthat lubridate shiny flexdashboard knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels xlsx lintr fs ggrepel";
 for PKG in $PACKAGE_LIST; do Rscript --vanilla -e "install.packages('$PKG', repos=c('https://cran.hafro.is/'))"; done;
 Rscript --vanilla -e "devtools::install_github('yorkulibraries/yulr')"
 Rscript --vanilla -e "devtools::install_github('docopt/docopt.R')"
