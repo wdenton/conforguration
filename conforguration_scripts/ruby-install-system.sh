c91a0c55cba93fd92e2ba249481423c2ca42a646
William Denton <wtd@pobox.com>
2019-05-30 10:14:04 -0400

    Ruby: move to 2.6.3

--- conforguration_scripts/ruby-install-system.sh
+++ conforguration_scripts/ruby-install-system.sh
@@ -2,7 +2,7 @@
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 unset RUBY_VERSION
-declare -a RUBY_VERSION=( '2.5.1' )
+declare -a RUBY_VERSION=( '2.6.3' )
 mkdir -p /usr/local/src/ruby
 cd /usr/local/src/ruby
 
