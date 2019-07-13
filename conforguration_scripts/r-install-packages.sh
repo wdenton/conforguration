f4fed2b715147066b4333e37308f83b1032e6b3d
William Denton <wtd@pobox.com>
2019-07-13 17:14:25 -0400

    R: 3.6.1; and add some packages

--- conforguration_scripts/r-install-packages.sh
+++ conforguration_scripts/r-install-packages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
-PACKAGE_LIST="tidyverse readxl devtools RCurl roxygen2 testthat lubridate shiny flexdashboard knitr ggvis seriation igraph arules arulesViz tm wordcloud cluster fpc topicmodels xlsx lintr fs ggrepel geonames maps mapproj ggridges";
+PACKAGE_LIST="tidyverse readxl devtools usethis remotes RCurl roxygen2 testthat lubridate shiny flexdashboard knitr ggvis \
+xlsx lintr fs ggrepel ggridges hms docopt \
+arules arulesViz tm wordcloud cluster fpc topicmodels seriation igraph \
+geonames maps mapproj rgeos sf maptools OpenStreetMap"
 for PKG in $PACKAGE_LIST; do Rscript --vanilla -e "install.packages('$PKG', repos=c('https://cran.hafro.is/'))"; done;
 Rscript --vanilla -e "devtools::install_github('yorkulibraries/yulr')"
-Rscript --vanilla -e "devtools::install_github('docopt/docopt.R')"
-Rscript --vanilla -e "devtools::install_github('tidyverse/hms')"
+# Rscript --vanilla -e "devtools::install_github('tidyverse/hms')"
