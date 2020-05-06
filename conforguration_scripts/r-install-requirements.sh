7853c70a5e3588faf377a8f6d4592cc0f1a1959e
William Denton <wtd@pobox.com>
2020-05-06 11:58:01 -0400

    R: curl is required

--- conforguration_scripts/r-install-requirements.sh
+++ conforguration_scripts/r-install-requirements.sh
@@ -3,6 +3,6 @@ script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 sudo apt-get install libbz2-dev liblzma-dev libxml2-dev libpcre2-dev libpcre3-dev fonts-inconsolata
 sudo apt-get install xorg-dev gfortran libreadline-dev libcurl4-openssl-dev libssl-dev libgsl-dev libcurl4-openssl-dev
-sudo apt-get build-dep r-base
+sudo apt-get curl build-dep r-base
 mkdir -p ~/R/history/
 mkdir -p /usr/local/src/R
