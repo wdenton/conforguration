f1cdee69c4ba4d16fe0e64e5702f115a6adc502c
William Denton <wtd@pobox.com>
2020-06-20 16:03:42 -0400

    Emacs: Require FiraCode font package

--- conforguration_scripts/emacs-install-requirements.sh
+++ conforguration_scripts/emacs-install-requirements.sh
@@ -2,7 +2,7 @@
 script_dir='conforguration_scripts'
 dotfiles_dir='dotfiles'
 sudo apt-get build-dep emacs
-sudo apt-get intall libjansson-dev valgrind
+sudo apt-get intall libjansson-dev valgrind fonts-firacode
 ## On Linux Mint, at least, these are not installed when the above is done.  Curious.
 ## On other systems, it can't hurt.
 sudo apt-get install texinfo install libxpm-dev libjpeg-dev libgif-dev libtiff-dev libtinfo-dev
