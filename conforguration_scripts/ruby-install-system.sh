e092a67bbca87a16b73e933e504a360dfedc7712
William Denton <wtd@pobox.com>
2021-03-16 18:25:37 -0400

    Ruby: Add http gem

--- conforguration_scripts/ruby-install-system.sh
+++ conforguration_scripts/ruby-install-system.sh
@@ -14,6 +14,6 @@ cd ruby-${RUBY_VERSION}
 make
 
 sudo make install
-sudo gem install marc nokogiri sqlite3 rubocop bundler rubyul docopt pry pry-doc
+sudo gem install marc nokogiri sqlite3 rubocop bundler rubyul docopt pry pry-doc http
 
 echo "Now run bundler install where needed."
