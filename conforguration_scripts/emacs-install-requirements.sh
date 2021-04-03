af851b6e3e3ca444d760d6b52b15f8970bc033bf
William Denton <wtd@pobox.com>
2021-04-03 14:31:57 -0400

    More refactoring of install scripts
    Removed script_dir variable.  It's hardcoded everywhere now.

--- conforguration_scripts/emacs-install-requirements.sh
+++ conforguration_scripts/emacs-install-requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir='conforguration_scripts'
 sudo apt-get build-dep emacs
-sudo apt-get intall libjansson-dev valgrind fonts-firacode
+sudo apt-get install libjansson-dev valgrind fonts-firacode
 ## On Linux Mint, at least, these are not installed when the above is done.  Curious.
 ## On other systems, it can't hurt.
-sudo apt-get install texinfo install libxpm-dev libjpeg-dev libgif-dev libtiff-dev libtinfo-dev
+sudo apt-get install texinfo libxpm-dev libjpeg-dev libgif-dev libtiff-dev libtinfo-dev
