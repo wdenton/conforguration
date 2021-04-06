6eae6c49f2e81353d61feceeffa2ac756592e111
William Denton <wtd@pobox.com>
2021-04-05 22:51:08 -0400

    Various tweaks and edits.

--- conforguration_scripts/r-install-packages.sh
+++ conforguration_scripts/r-install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PACKAGE_LIST="RCurl arules arulesViz cluster devtools docopt flexdashboard fpc fs geonames geosphere ggrepel ggridges ggvis hms igraph knitr lintr lubridate mapproj maps maptools osmdata readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
-for PKG in $PACKAGE_LIST
+package_list="RCurl arules arulesViz cluster devtools docopt flexdashboard fpc fs geonames geosphere ggrepel ggridges ggvis hms igraph knitr lintr lubridate mapproj maps maptools osmdata readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
+for pkg in $package_list
 do
-    Rscript --vanilla -e "install.packages('$PKG', repos=c('https://utstat.toronto.edu/cran/'))"
+    Rscript --vanilla -e "install.packages('$pkg', repos=c('https://utstat.toronto.edu/cran/'))"
 done
 Rscript --vanilla -e "devtools::install_github('yorkulibraries/yulr')"
