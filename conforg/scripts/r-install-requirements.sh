3f3f61a0a5282b9dc300f5520e4f5f449f404a2e
William Denton <wtd@pobox.com>
2021-08-18 22:40:53 -0400

    R: And still more fixes to install script

--- conforg/scripts/r-install-requirements.sh
+++ conforg/scripts/r-install-requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install libbz2-dev liblzma-dev libxml2-dev libpcre2-dev libpcre3-dev fonts-inconsolata
 sudo apt install xorg-dev gfortran libreadline-dev libcurl4-openssl-dev libssl-dev libgsl-dev curl libcurl4-openssl-dev
-sudo apt install build-dep r-base
+sudo apt build-dep r-base
 mkdir -p ~/R/history/
 mkdir -p /usr/local/src/R
