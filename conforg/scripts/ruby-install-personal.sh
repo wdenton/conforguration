b93d3ea5af3adacfc79b9a719af9c489aeabda7c
William Denton <wtd@pobox.com>
2021-08-23 12:26:00 -0400

    Ruby: 3.0.2, and fix download for system-wide install

--- conforg/scripts/ruby-install-personal.sh
+++ conforg/scripts/ruby-install-personal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset RUBY_VERSION
-declare -a RUBY_VERSION=( '3.0.1' )
+declare -a RUBY_VERSION=( '3.0.2' )
 rbenv install --verbose $RUBY_VERSION
 rbenv global $RUBY_VERSION
 ~/conforg/scripts/ruby-install-gems.sh
