d52ed08d2ab997c0796e80996d2a297b5387ffd6
William Denton <wtd@pobox.com>
2021-09-14 21:33:48 -0400

    R: Add readODS package

--- conforg/scripts/r-install-packages.sh
+++ conforg/scripts/r-install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-package_list="RCurl arules arulesViz cluster devtools docopt flexdashboard fpc fs geonames geosphere ggrepel ggridges ggvis hms igraph knitr lintr lubridate mapproj maps maptools osmdata readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
+package_list="RCurl arules arulesViz cluster devtools docopt flexdashboard fpc fs geonames geosphere ggrepel ggridges ggvis hms igraph knitr lintr lubridate mapproj maps maptools osmdata readODS readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
 for pkg in $package_list
 do
     Rscript --vanilla -e "install.packages('$pkg', repos=c('https://utstat.toronto.edu/cran/'))"
