0ae3599293f2923c40c6a3b5d0e53ff154b1d21b
William Denton <wtd@pobox.com>
2023-01-06 13:58:50 -0500

    Ruby: version 3.2

--- conforg/scripts/ruby-install-personal.sh
+++ conforg/scripts/ruby-install-personal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset RUBY_VERSION
-declare -a RUBY_VERSION=( '3.1.1' )
+declare -a RUBY_VERSION=( '3.2.0' )
 rbenv install --verbose $RUBY_VERSION
 rbenv global $RUBY_VERSION
 ~/conforg/scripts/ruby-install-gems.sh
