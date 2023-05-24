a85c92afeba66e3530393cb76fc90de6085a42ad
William Denton <wtd@pobox.com>
2023-05-24 12:02:22 -0400

    Emacs: Don't update Org if Emacs compilation failed

--- conforg/scripts/emacs-update-native-compilation.sh
+++ conforg/scripts/emacs-update-native-compilation.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /usr/local/src/emacs/
 git pull
-./configure --with-json --with-pgtk --with-tree-sitter --with-native-compilation && make -j 8
-cd ../org-mode/
-make -j 8 update
+./configure --with-json --with-pgtk --with-tree-sitter --with-native-compilation && make -j 8 && cd ../org-mode/ && make -j 8 update
--- conforg/scripts/emacs-update.sh
+++ conforg/scripts/emacs-update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /usr/local/src/emacs/
 git pull
-./configure --with-json --with-pgtk --with-tree-sitter && make -j 8
-cd ../org-mode/
-make -j 8 update
+./configure --with-json --with-pgtk --with-tree-sitter && make -j 8 && cd ../org-mode/ && make -j 8 update
