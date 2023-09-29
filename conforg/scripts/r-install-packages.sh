f2be804447524624d234a0559b33f26d5b39f754
William Denton <wtd@pobox.com>
2023-09-29 16:01:07 -0400

    R: add openxlsx package

--- conforg/scripts/r-install-packages.sh
+++ conforg/scripts/r-install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-package_list="RCurl RSQLite arules arulesViz cluster dbplyr devtools docopt flexdashboard fpc fs geonames geosphere ggrepel ggridges ggvis gtools hms igraph knitr lintr lubridate mapproj maps maptools osmdata readODS readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
+package_list="RCurl RSQLite arules arulesViz cluster dbplyr devtools docopt flexdashboard fpc fs geonames geosphere GGally ggrepel ggridges ggvis gtools hms igraph knitr lintr lubridate mapproj maps maptools openxlsx osmdata readODS readxl remotes rgeos roxygen2 seriation sf shiny skimr testthat tidyverse tm topicmodels usethis xlsx"
 for pkg in $package_list
 do
     Rscript --vanilla -e "install.packages('$pkg', repos=c('https://mirror.csclub.uwaterloo.ca/CRAN/'))"
