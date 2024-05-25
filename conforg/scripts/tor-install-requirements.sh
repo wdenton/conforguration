d4aa4d37018b2487826811c30c5ba04bf29de13f
William Denton <william@williamdenton.org>
2024-05-25 14:33:03 -0400

    Tor: tor-run.sh now has four shells, one running vnstat

--- conforg/scripts/tor-install-requirements.sh
+++ conforg/scripts/tor-install-requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install build-essential
 sudo apt install libevent-dev libssl-dev libcap-dev liblzma-dev libzstd-dev libseccomp-dev libscrypt-dev zlib1g zlib1g-dev
-sudo apt install speedometer tmux
+sudo apt install speedometer tmux vnstat
 mkdir -p /usr/local/src/tor/
--- conforg/scripts/tor-run.sh
+++ conforg/scripts/tor-run.sh
@@ -5,10 +5,12 @@ if tmux has-session -t tor
      tmux send-keys -t tor:1 "C-c"
      tmux send-keys -t tor:2 "C-c"
      tmux send-keys -t tor:3 "C-c"
+     tmux send-keys -t tor:4 "C-c"
      sleep 35
      tmux kill-window -t tor:1
      tmux kill-window -t tor:2
      tmux kill-window -t tor:3
+     tmux kill-window -t tor:4
  fi
 
 echo "Starting tor session ..."
@@ -20,10 +22,14 @@ if [[ -z $TOR_IFACE ]]; then
 fi
 
 tmux new-window -t tor:2
-tmux send-keys -t tor:2 "speedometer -t ${TOR_IFACE} -r ${TOR_IFACE} -l -m 1048576" "C-m"
+tmux new-window -t tor:3
+tmux new-window -t tor:4
+
+tmux send-keys -t tor:3 "speedometer -t ${TOR_IFACE} -r ${TOR_IFACE} -l -m 1048576" "C-m"
 
 tmux select-window -t 1
 tmux send-keys -t tor:1 "tor" "C-m"
 
-tmux new-window -t tor:3
-tmux send-keys -t tor:3 "tail -f /usr/local/src/tor/log/notices.log" "C-m"
+tmux send-keys -t tor:2 "tail -f /usr/local/src/tor/log/notices.log" "C-m"
+
+tmux send-keys -t tor:4 "vnstat -d --iface ${TOR_IFACE}" "C-m"
