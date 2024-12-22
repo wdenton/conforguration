6df9affc924e60df8323e247e248a39371440457
William Denton <william@williamdenton.org>
2024-12-22 12:44:12 -0500

    Tor: create log directory

--- conforg/scripts/tor-install-requirements.sh
+++ conforg/scripts/tor-install-requirements.sh
@@ -2,4 +2,4 @@
 sudo apt install build-essential
 sudo apt install libevent-dev libssl-dev libcap-dev liblzma-dev libzstd-dev libseccomp-dev libscrypt-dev zlib1g zlib1g-dev
 sudo apt install speedometer tmux vnstat
-mkdir -p /usr/local/src/tor/
+mkdir -p /usr/local/src/tor/ /usr/local/src/tor/log/
