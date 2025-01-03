dfd7dd4bbde311b0680df9935329a1a20991d6a9
William Denton <william@williamdenton.org>
2025-01-03 15:31:21 -0500

    R: bump to 4.4.2

--- conforg/scripts/r-install-system.sh
+++ conforg/scripts/r-install-system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset R_VERSION
-declare -a R_VERSION=( '4.4.1' )
+declare -a R_VERSION=( '4.4.2' )
 cd $(dirname "$0")
 SCRIPTS_DIR=$(pwd)
 MIRROR="https://mirror.csclub.uwaterloo.ca/CRAN/"
